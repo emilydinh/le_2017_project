e3566088afb26b35c820b392cfe3778c639c6067
Emily Dinh <emilydinh@ufl.edu>
2017-11-14 13:43:14 -0500

    Read in cattle egret avian malaria FASTA file & sequences. We're now trying to find a way to calculate sequence divergence (see link in line 10).

--- cattle_egret_malaria_conn.R
+++ cattle_egret_malaria_conn.R
@@ -1,1 +1,10 @@
 # Here's a blank (for now) script
+library("ShortRead")
+
+# read in cattle egret avian malaria sequences FASTA file
+caeg_avimal <- readFasta("CaEg_AviMal_seq.fasta")
+# read sequences from FASTA file
+seqs <- sread(caeg_avimal)
+
+# Possible way of calculating sequence divergence:
+https://www.bioconductor.org/packages/devel/bioc/vignettes/seqcombo/inst/doc/seqcombo.html
