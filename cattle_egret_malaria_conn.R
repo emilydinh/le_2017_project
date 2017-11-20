bac47ff9421736cab6c2f3fcc5b8561de0a9cace
Dinh <emilydinh@ufl.edu>
2017-11-20 13:58:26 -0500

    I'm trying to create pie charts for each place, but I'm not successful yet. Still playing around with how to do that.

--- cattle_egret_malaria_conn.R
+++ cattle_egret_malaria_conn.R
@@ -2,7 +2,6 @@
 
 
 # Step 1 - set up data
-
 # Import lineages & GPS data
 lineages_data <- read.csv("blast_lineages_updated.csv")
 gps_loc <- read.csv("Location GPS.csv")
@@ -23,8 +22,74 @@ lineage_loc <- merge(lineage_count, gps_loc, by="General.Capture.Location")
 
 
 
-###########################################################################################################################
+# Step 2 - Create map
 
+# Load packages
+library(raster)
+library(rgdal)
+library(ggplot2)
+library(ggrepel)
+
+#Import state map of FL
+FL_map <- readOGR(dsn=".", layer="Florida")
+
+# Create map
+ggplot() +
+  theme_bw() +
+  theme(
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(colour = "black"),
+    axis.line.x = element_line(color = "black", size = 0.25),
+    axis.line.y = element_line(color = "black", size = 0.25)) +
+  scale_x_continuous(limits = c(-90, -74)) +
+  scale_y_continuous(limits = c(24, 35)) +
+  coord_fixed() +
+  # Draw FL border
+  geom_polygon(
+    data = FL_map,
+    aes(x = long, y = lat, group = group),
+    fill = NA,
+    col = "black") +
+  # Plot the sites, make grey points for now
+  geom_point(
+    data = lineage_loc,
+    aes(x = Long, y = Lat, fill="General.Capture.Location"), 
+    size = 3.0, pch = 16, color="grey") +
+  # Reformat the legend from having multiple points to 1 point representing capture locations. NOTE: can't figure out how to properly change
+  # label of point from General.Capture.Location to Capture Location
+  scale_fill_manual(name="Legend", values=c("Capture Location" = "grey")) +
+  # Plot the count of each lineage for each site with at least one sample
+  bar <- ggplot(lineage_loc, aes(x=General.Capture.Location, y=freq, fill=Lineage)) +
+  geom_bar(width=1, stat="identity")
+
+  pie <- bar + coord_polar(theta="y", start=0) + theme(axis.text = element_blank())
+  plot(pie)
+  
+  
+  
+  geom_text_repel(
+    data = lineage_loc,
+    aes(x = Long, y = Lat, label = freq),
+    fontface = "bold",
+    col = "black",
+    size = 4,
+    segment.color = "blue",
+    box.padding = unit(0.3, "lines"))
+
+# Playing around with how to make a pie chart of lineages for each location
+  bar <- ggplot(lineage_loc, aes(x=General.Capture.Location, y=freq, fill=Lineage)) +
+    geom_bar(width=1, stat="identity")
+  
+  pie <- bar + coord_polar(theta="y", start=0) + theme(axis.text = element_blank())
+  plot(pie)
+
+
+
+
+###########################################################################################################################
+lineage_loc$General.Capture.Location[1]
 
 # read in cattle egret avian malaria sequences FASTA file
 library("ShortRead")
