7274db5bb8bdddea50375f0464b4aadd09c92820
Dinh <emilydinh@ufl.edu>
2017-11-20 12:18:20 -0500

    I'm beginning to code for the map of lineage locations in FL. I've imported the updated BLAST lineages & GPS locations, then merged the 2 data frames together to produce a one data frame with sequence lineages by location.

--- cattle_egret_malaria_conn.R
+++ cattle_egret_malaria_conn.R
@@ -1,7 +1,29 @@
-# Here's a blank (for now) script
-library("ShortRead")
+# Mapping lineage locations
+
+
+# Step 1 - set up data
+
+# Import lineages & GPS data
+lineages_data <- read.csv("blast_lineages_updated.csv")
+gps_loc <- read.csv("Location GPS.csv")
+
+# Rename "Location" column in lineages_data to "General.Capture.Location" to match to gps_loc for merging
+library(plyr)
+lineages_data <- rename(x=lineages_data, replace=c("Location"="General.Capture.Location"))
+
+# Merge lineages_data & gps_loc by General.Capture.Location to get sequence lineages by location
+seq_loc_data <- merge(x=lineages_data, y=gps_loc, by="General.Capture.Location")
+
+
+
+
+
+
+###########################################################################################################################
+
 
 # read in cattle egret avian malaria sequences FASTA file
+library("ShortRead")
 caeg_avimal <- readFasta("CaEg_AviMal_seq.fasta")
 # read sequences from FASTA file
 seqs <- sread(caeg_avimal)
