a87c1de5a5d9137de944d4fb4f2a3eb5aaca969d
Emily Dinh <emilydinh@ufl.edu>
2017-11-27 22:41:57 -0500

    I included negative results in the preliminary pie chart & removed the species name from Haemosporidia novel.

--- cattle_egret_malaria_conn.R
+++ cattle_egret_malaria_conn.R
@@ -3,29 +3,30 @@
 
 # Step 1 - set up data
 # Import lineages & GPS data
-lineages_data <- read.csv("blast_lineages_updated.csv")
+lin_data <- read.csv("blast_lineages_neg.csv")
 gps_loc <- read.csv("Location GPS.csv")
 
-# Change AY-2014 lineage to Haemosporidia novel
+# Change AY-2014 lineage to Haemosporidia novel & remove species name associated with it
 # First convert Lineage to character from factor
-lineages_data$Lineage <- as.character(lineages_data$Lineage)
-lineages_data$Lineage[lineages_data$Lineage=="AY-2014"] <- "Haemosporidia novel"
+lin_data$Lineage <- as.character(lin_data$Lineage)
+lin_data$Lineage[lin_data$Lineage=="AY-2014"] <- "Haemosporidia novel"
+lin_data$Species[lin_data$Lineage=="Haemosporidia novel"] <- ""
 
-# Rename "Location" column in lineages_data to "General.Capture.Location" to match to gps_loc for merging
+# Rename "Location" column in lin_data to "General.Capture.Location" to match to gps_loc for merging
 library(plyr)
-lineages_data <- rename(x=lineages_data, replace=c("Location"="General.Capture.Location"))
+lin_data <- rename(x=lin_data, replace=c("Location"="General.Capture.Location"))
 
-# Merge "Species" & "Lineage" columns in lineages_data
-lineages_data$Sp.Lin <- paste(lineages_data$Species, lineages_data$Lineage)
+# Merge "Species" & "Lineage" columns in lin_data
+lin_data$Sp.Lin <- paste(lin_data$Species, lin_data$Lineage)
 
-# Merge lineages_data & gps_loc by General.Capture.Location to get sequence lineages by location
-seq_loc_data <- merge(x=lineages_data, y=gps_loc, by="General.Capture.Location")
+# Merge lin_data & gps_loc by General.Capture.Location to get lineages by location
+lin_loc_data <- merge(x=lin_data, y=gps_loc, by="General.Capture.Location")
 
 # Count the number of each lineage for each location
-lineage_count <- count(seq_loc_data, vars = c("Sp.Lin","General.Capture.Location"))
+lin_count <- count(lin_loc_data, vars = c("Sp.Lin","General.Capture.Location"))
 
-# Merge the count data frame to gps_loc for number of each lineage/location
-lineage_loc <- merge(lineage_count, gps_loc, by="General.Capture.Location")
+# Merge the count data frame to gps_loc to get number of each lineage/location
+lin_loc <- merge(lin_count, gps_loc, by="General.Capture.Location")
 
 
 
@@ -61,18 +62,18 @@ ggplot() +
     col = "black") +
   # Plot the sites, make points & point labels blue, bold for now
   geom_point(
-    data = lineage_loc,
+    data = lin_loc,
     aes(x = Long, y = Lat), 
     size = 3.0, pch = 16, color="blue") +
-  geom_text(data=lineage_loc,aes(x=Long, y=Lat, label=General.Capture.Location), color="blue", nudge_x=0.1, nudge_y=-0.15, fontface="bold")
+  geom_text(data=lin_loc,aes(x=Long, y=Lat, label=General.Capture.Location), color="blue", nudge_x=0.1, nudge_y=-0.15, fontface="bold")
   # Save plot. I can't fugure out how to put pie charts + map together, so I'll do them separately for now.
   ggsave("prelim_map.jpg", plot = last_plot(), dpi = 300, width = 20, height = 20, units = "cm")
 
 
 
   # Plot the count of each lineage for each site; doing 1 sample pie chart (Homestead) for now
-  bar <- ggplot(data=lineage_loc, aes(x=General.Capture.Location, y=freq, fill=Sp.Lin)) +
-  geom_bar(data=subset(lineage_loc, General.Capture.Location=="Homestead"), width=1, stat="identity") +
+  bar <- ggplot(data=lin_loc, aes(x=General.Capture.Location, y=freq, fill=Sp.Lin)) +
+  geom_bar(data=subset(lin_loc, General.Capture.Location=="Homestead"), width=1, stat="identity") +
   labs(fill= "Species & Lineage")
 
   pie <- bar + coord_polar(theta="y") + theme_minimal() +
